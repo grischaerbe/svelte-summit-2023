1a0edcde62befd5bcea9989113d43c3f971c8a75
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-05-05 10:40:16 +0200

    initial commit

---
+++ src/routes/Scene.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { T, useFrame } from '@threlte/core'
+	import { Environment, Text } from '@threlte/extras'
+	import Ticket from './Ticket.svelte'
+
+	let rotationY = 0
+
+	useFrame((_, delta) => {
+		rotationY += 0.1 * delta
+	})
+</script>
+
+<T.PerspectiveCamera
+	makeDefault
+	fov={10}
+	position={[0, 0, 40]}
+/>
+
+<Environment files="/oil-on-water.png" />
+
+<T.Group rotation.y={rotationY}>
+	<Text
+		text="Max Mustermann"
+		font="/Flama.otf"
+		fontSize={0.25}
+		position={[-0.8, -0.35, 0.04]}
+		maxWidth={1.5}
+	>
+		<T.MeshStandardMaterial envMapIntensity={11} />
+	</Text>
+
+	<Text
+		text="NO. 12345"
+		font="/Flama.otf"
+		fontSize={0.25}
+		position={[0, 1.88, 0.04]}
+		anchorX="50%"
+		anchorY="50%"
+	>
+		<T.MeshStandardMaterial
+			envMapIntensity={11}
+			color="black"
+			roughness={0}
+		/>
+	</Text>
+
+	<Ticket />
+</T.Group>
